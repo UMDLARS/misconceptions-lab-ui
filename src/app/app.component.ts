a3a611f545369e2861f934a43a9ff763a62aecec
Brandon Geraci <bran0847@d.umn.edu>
2019-06-20 15:21:01 -0500

    Added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,62 @@ export class AppComponent {
       title: 'EnCRAPter',
       link: 'encrapter',
     },
+    {
+      title: 'Anonymized-data',
+      link: 'anonymized',
+    },
+    {
+      title: 'CompletelyAnonymous',
+      link: 'completelyanonymous',
+    },
+    {
+      title: 'defenseindepth',
+      link: 'defenseindepth',
+    },
+    {
+      title: 'encryptionautointegrity',
+      link: 'encryptionautointegrity',
+    },
+    {
+      title: 'goodpasswords',
+      link: 'goodpasswords',
+    },
+    {
+      title: 'humansarerational',
+      link: 'humansarerational',
+    },
+    {
+      title: 'notatarget',
+      link: 'notatarget',
+    },
+    {
+      title: 'physicalsecruity',
+      link: 'ephysicalsecruity',
+    },
+    {
+      title: 'privacynotimportant',
+      link: 'privacynotimportant',
+    },
+    {
+      title: 'securitybyobscurity',
+      link: 'securitybyobscurity',
+    },
+    {
+      title: 'securityinmind',
+      link: 'securityinmind',
+    },
+    {
+      title: 'secruityproduct',
+      link: 'secruityproduct',
+    },
+    {
+      title: 'trustusers',
+      link: 'trustusers',
+    },
+    {
+      title: 'twofa',
+      link: 'twofa',
+    },
   ];
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,21 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NbEvaIconsModule} from '@nebular/eva-icons';
 import {EncrapterComponent} from './encrapter/encrapter.component';
 import { HomeComponent } from './home/home.component';
+import { PhysicalsecurityComponent } from './physicalsecurity/physicalsecurity.component';
+import { NotatargetComponent } from './notatarget/notatarget.component';
+import { GoodpasswordsComponent } from './goodpasswords/goodpasswords.component';
+import { CompletelyanonymousComponent } from './completelyanonymous/completelyanonymous.component';
+import { SecurityinmindComponent } from './securityinmind/securityinmind.component';
+import { SecurityproductComponent } from './securityproduct/securityproduct.component';
+import { HumansarerationalComponent } from './humansarerational/humansarerational.component';
+import { TrustUsersComponent } from './trust-users/trust-users.component';
+import { AnonymizedDataComponent } from './anonymized-data/anonymized-data.component';
+import { SecurityByObscurityComponent } from './security-by-obscurity/security-by-obscurity.component';
+import { DefenseInDepthComponent } from './defense-in-depth/defense-in-depth.component';
+import { UsersAreNotMaliciousComponent } from './users-are-not-malicious/users-are-not-malicious.component';
+import { PrivacyNotImportantComponent } from './privacy-not-important/privacy-not-important.component';
+import { EncryptionAutoIntegrityComponent } from './encryption-auto-integrity/encryption-auto-integrity.component';
+import { TwoFAComponent } from './two-fa/two-fa.component';
 
 const appRoutes: Routes = [
   {
@@ -27,6 +42,19 @@ const appRoutes: Routes = [
   },
   {path: 'home', component: HomeComponent},
   {path: 'encrapter', component: EncrapterComponent},
+  {path: 'anonymized', component: AnonymizedDataComponent},
+  {path: 'completlyanonymous', component: CompletelyanonymousComponent},
+  {path: 'defenseindepth', component: DefenseInDepthComponent},
+  {path: 'encryptionautointegrity', component: EncryptionAutoIntegrityComponent},
+  {path: 'goodpasswords', component: GoodpasswordsComponent},
+  {path: 'humansarerational', component: HumansarerationalComponent},
+  {path: 'notatarget', component: NotatargetComponent},
+  {path: 'physicalsecruity', component: PhysicalsecurityComponent},
+  {path: 'privacynotimportant', component: PrivacyNotImportantComponent},
+  {path: 'securitybyobscurity', component: SecurityByObscurityComponent},
+  {path: 'securityinmind', component: SecurityinmindComponent},
+  {path: 'secruityproduct', component: SecurityproductComponent},
+  {path: 'trustusers', component: TrustUsersComponent},
 ];
 
 @NgModule({
@@ -34,6 +62,21 @@ const appRoutes: Routes = [
     AppComponent,
     EncrapterComponent,
     HomeComponent,
+    PhysicalsecurityComponent,
+    NotatargetComponent,
+    GoodpasswordsComponent,
+    CompletelyanonymousComponent,
+    SecurityinmindComponent,
+    SecurityproductComponent,
+    HumansarerationalComponent,
+    TrustUsersComponent,
+    AnonymizedDataComponent,
+    SecurityByObscurityComponent,
+    DefenseInDepthComponent,
+    UsersAreNotMaliciousComponent,
+    PrivacyNotImportantComponent,
+    EncryptionAutoIntegrityComponent,
+    TwoFAComponent,
   ],
   imports: [
     BrowserModule,
