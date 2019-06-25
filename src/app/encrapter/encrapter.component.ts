74b70dcedad4fac599d9553ff8f94c74a063a6ee
Josie K <thorntheletter@gmail.com>
2019-06-25 12:07:30 -0500

    attempting to get tables again

--- src/app/encrapter/encrapter.component.ts
+++ src/app/encrapter/encrapter.component.ts
@@ -8,6 +8,9 @@ import {Component, OnInit} from '@angular/core';
 export class EncrapterComponent implements OnInit {
   plaintext: string;
   ciphertext = '';
+  plaintable = {}
+  ciphertable = {}
+  esel: string;
   selectedCipher = 'rot13';
 
   constructor() {
@@ -15,12 +18,64 @@ export class EncrapterComponent implements OnInit {
 
   ngOnInit() {
   }
-
-  updateCiphertext() {
+  rot13(instring){
     const input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     const output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
     const index = x => input.indexOf(x);
     const translate = x => index(x) > -1 ? output[index(x)] : x;
-    this.ciphertext = this.plaintext.split('').map(translate).join('');
+    return instring.split('').map(translate).join('');
   }
-}
+
+  rever(instring){
+    for(var i = 0; i < instring.length; i++){
+      var c = instring.charCodeAt(i);
+      c = String.fromCharCode(c ^ 64);
+      instring = instring.substr(0,i) + c + instring.substr(i+1);
+    }
+    return instring.split("").reverse().join("");
+  }
+
+  onetp(instring){
+    return "onetp";
+  }
+
+  changeEncryptionType(inarg){
+    this.selectedCipher = inarg;
+    this.updateCiphertext();
+  }
+
+
+  updateCiphertext() {
+    var instring = this.plaintext;
+    var out = ""
+    console.log(this.selectedCipher);
+    switch (this.selectedCipher){
+      case "rot13":
+	console.log("rot13");
+	out = this.rot13(instring);
+	break;
+      case "rever":
+	console.log("2")
+	out = this.rever(instring);
+	break;
+      case "onetp":
+	console.log("3");
+	out = this.onetp(instring);
+	break;
+    }
+    this.ciphertext = out;
+    this.plaintable = this.dictgen(instring);
+    console.log(this.plaintable['a']);
+  }
+
+  dictgen(instring){
+    var dict = {};
+      for(var i = 0; i < instring.length; i++){
+        if(!dict[instring.charAt(i)]){
+          dict[instring.charAt(i)] = 0;
+        }
+      dict[instring.charAt(i)] += 1;
+      }
+    return(dict)
+    }
+  }
