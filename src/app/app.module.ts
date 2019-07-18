14a5f56119e5a09d57a7f5c4f64d398c58cce344
jnowaczek <jnowaczek@users.noreply.github.com>
2019-07-17 22:29:15 -0500

    Add `ng2-smart-table` and implement for Anonymization lab Refactor CSV data to array literal for optionals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,7 @@ import {PrivacyNotImportantComponent} from './privacy-not-important/privacy-not-
 import {EncryptionAutoIntegrityComponent} from './encryption-auto-integrity/encryption-auto-integrity.component';
 import {TwoFAComponent} from './two-fa/two-fa.component';
 import {ROUTES} from './routes';
+import {Ng2SmartTableModule} from 'ng2-smart-table';
 
 @NgModule({
   declarations: [
@@ -87,6 +88,7 @@ import {ROUTES} from './routes';
     NbActionsModule,
     NbIconModule,
     NbSelectModule,
+    Ng2SmartTableModule,
   ],
   providers: [NbMenuService],
   bootstrap: [AppComponent]
