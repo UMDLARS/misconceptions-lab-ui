94166ed59ce55d72c84f5cb8c1b080b71522b248
Brandon Geraci <bran0847@d.umn.edu>
2019-07-19 21:19:03 -0500

    Changed up the flask app. Websockets are handled locally on the flask web app.

--- src/app/notatarget/notatarget.component.ts
+++ src/app/notatarget/notatarget.component.ts
@@ -21,10 +21,10 @@ export class NotatargetComponent implements OnInit, OnDestroy {
   thiscontainer: any;
 
   constructor(private  http: HttpClient) {
-    const url = 'http://10.1.1.12:5000';
-    this.http.get(url).subscribe(
-      data => this.doSomething(data)
-    );
+    // const url = 'http://10.1.1.12:5000';
+    // this.http.get(url).subscribe(
+    //   data => this.doSomething(data)
+    // );
   }
 
 
@@ -33,37 +33,37 @@ export class NotatargetComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    console.log('AAAAAAHHHHH');
-    this.http.post('http://10.1.1.12:5000/destroy',
-      {
-        container:  this.thiscontainer
-      })
-      .subscribe(
-        data  => {
-          console.log('POST Request is successful ', data);
-        },
-        error  => {
-
-          console.log('Error', error);
-
-        }
-
-      );
+    // console.log('AAAAAAHHHHH');
+    // this.http.post('http://10.1.1.12:5000/destroy',
+    //   {
+    //     container:  this.thiscontainer
+    //   })
+    //   .subscribe(
+    //     data  => {
+    //       console.log('POST Request is successful ', data);
+    //     },
+    //     error  => {
+    //
+    //       console.log('Error', error);
+    //
+    //     }
+    //
+    //   );
   }
 
-  doSomething(data) {
-    this.thisdocker = data;
-    this.thisurl = data.url;
-    this.ifrm = document.createElement('iframe');
-    this.ifrm.setAttribute('id', 'ifrm');
-    this.ifrm.setAttribute('src', this.thisurl);
-    this.ifrm.setAttribute('height', '1000px');
-    this.ifrm.setAttribute('width', '100%');
-    this.ifrm.setAttribute('style', 'border:none;');
-    let el = document.getElementById('vm');
-    el.parentNode.append(this.ifrm);
-    this.thiscontainer = data.container;
-  }
+  // doSomething(data) {
+  //   this.thisdocker = data;
+  //   this.thisurl = data.url;
+  //   this.ifrm = document.createElement('iframe');
+  //   this.ifrm.setAttribute('id', 'ifrm');
+  //   this.ifrm.setAttribute('src', this.thisurl);
+  //   this.ifrm.setAttribute('height', '1000px');
+  //   this.ifrm.setAttribute('width', '100%');
+  //   this.ifrm.setAttribute('style', 'border:none;');
+  //   let el = document.getElementById('vm');
+  //   el.parentNode.append(this.ifrm);
+  //   this.thiscontainer = data.container;
+  // }
 
   onThirdSubmit() {
   }
