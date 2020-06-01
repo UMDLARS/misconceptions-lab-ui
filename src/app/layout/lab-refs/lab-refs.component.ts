9cde199f205cb1ff650d786a159f5d41d04794a3
power327 <power327@d.umn.edu>
2020-06-01 11:24:59 -0500

    Added references card.  Refs is not automatically added to every lab, but is available if desired.
    Changes to NaT are just test material.  Nothing meaningful.

---
+++ src/app/layout/lab-refs/lab-refs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lab-refs',
+  templateUrl: './lab-refs.component.html',
+  styleUrls: ['./lab-refs.component.css']
+})
+export class LabRefsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
